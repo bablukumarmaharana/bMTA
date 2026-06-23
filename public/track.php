b5421ce0239bbb5a5700de3410c13dea4749759d
Bablu Kumar Maharana <29585219+bablukumarmaharana@users.noreply.github.com>
2026-06-23 22:05:17 +0530

    Update track.php

--- public/track.php
+++ public/track.php
@@ -1,10 +1,9 @@
 <?php
-require_once __DIR__ . '/../src/Tracking.php';
 require_once __DIR__ . '/../src/Database.php';
+require_once __DIR__ . '/../src/Tracking.php';
 
 $config = require __DIR__ . '/../config/config.php';
 $db = new Database($config['db']);
-
 $tracking = new Tracking($db);
 
 $type = $_GET['type'] ?? '';
@@ -12,7 +11,7 @@ $id   = $_GET['id'] ?? '';
 
 if ($type === 'open' && $id) {
     $tracking->logOpen($id, $_SERVER['REMOTE_ADDR'] ?? '', $_SERVER['HTTP_USER_AGENT'] ?? '');
-    // Output 1x1 transparent GIF
+    // Transparent 1x1 GIF
     header('Content-Type: image/gif');
     echo base64_decode('R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');
     exit;
@@ -25,4 +24,4 @@ if ($type === 'click' && $id && isset($_GET['url'])) {
 }
 
 http_response_code(400);
-echo 'Invalid tracking request';
+echo 'Invalid request';
